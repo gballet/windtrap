34e1ec79c1461e2d19d3f115eb2a2006c12888b4
Guillaume Ballet <gballet@gmail.com>
2018-09-19 19:37:09 +0200

    Add some tests and decode the import section

--- lib/windtrap.ex
+++ lib/windtrap.ex
@@ -6,14 +6,15 @@ defmodule Windtrap do
 	@wasm_header (<<0>> <> "asm" <> << 1 ::little-size(32)>>)
 
 	@section_types_id 1
+	@section_imports_id 2
 
 	@doc """
 	Decode a wasm binary.
 
 	## Examples
 
-	  iex> Windtrap.decode(filename)
-	  %Windtrap.Module{...}
+	  iex> %Windtrap.Module{} = Windtrap.decode("filename")
+	  %Windtrap.Module{}
 	"""
 	def decode(filename) do
 		{:ok, data} = File.read(filename)
@@ -33,12 +34,76 @@ defmodule Windtrap do
 		|> decode_data
 	end
 
-	defp varint_size(<<x, rest :: binary>>) when x < 128 do
+	def varint_size(<<x, rest :: binary>>) when x < 128 do
 		{x, rest}
 	end
-	defp varint_size(<<x, rest :: binary>>) when x >= 128 do
+	def varint_size(<<x, rest :: binary>>) when x >= 128 do
 		{z, r} = varint_size(rest)
 		{x-128 + 128*z, r}
+	end
+
+	def vec(<<payload :: binary>>) do
+		{size, rest} = varint_size(payload)
+		vec_unfurl({}, size, rest)
+	end
+	defp vec_unfurl(v, s, <<payload :: binary>>) when s > 0 do
+		{item, rest} = vec_item payload
+		vec_unfurl Tuple.append(v, item), (s-1), rest
+	end
+	defp vec_unfurl(v, 0, r), do: {v, r}
+
+	defp vec_item(<<0x7c, rest :: binary>>), do: {:f64, rest}
+	defp vec_item(<<0x7d, rest :: binary>>), do: {:f32, rest}
+	defp vec_item(<<0x7e, rest :: binary>>), do: {:i64, rest}
+	defp vec_item(<<0x7f, rest :: binary>>), do: {:i32, rest}
+	defp vec_item(<<0x40, rest  :: binary>>), do: {{}, rest}
+	defp vec_item(<<0x60, rest  :: binary>>) do
+		{args, r1} = vec(rest)
+		{result, r2} = vec(r1)
+		{{args, result}, r2}
+	end
+
+	# TODO simplify this to avoid code duplication
+	def import_vec(<<payload :: binary>>) do
+		{size, rest} = varint_size(payload)
+		import_vec_unfurl({}, size, rest)
+	end
+	defp import_vec_unfurl(v, s, <<payload :: binary>>) when s > 0 do
+		{item, rest} = import_vec_item payload
+		import_vec_unfurl Tuple.append(v, item), (s-1), rest
+	end
+	defp import_vec_unfurl(v, 0, r), do: {v, r}
+	defp import_vec_item(<<p::binary>>) do
+		{msize, mrest} = varint_size(p)
+		<<mname::binary-size(msize), q::binary>> = mrest
+		{isize, irest} = varint_size(q)
+		<<iname::binary-size(isize), q::binary>> = irest
+		import_vec_item_type %{mod: mname, import: iname}, q
+	end
+	defp import_vec_item_type(t, <<0x3, type, constvar, p::binary>>) do
+		{Map.merge(t, %{type: :global, const: constvar == 0, valtype: type}), p}
+	end
+	defp import_vec_item_type(t, <<0x0, p::binary>>) do
+		{idx, r} = varint_size(p)
+		{Map.merge(t, %{type: :typeidx, index: idx}), r}
+	end
+	defp import_vec_item_type(t, <<0x2, has_max, p::binary>>) do
+		{min, r} = varint_size(p)
+		if has_max == 1 do
+			{max, s} = varint_size(r)
+			{Map.merge(t, %{type: :memory, min: min, max: max}), s}
+		else
+			{Map.merge(t, %{type: :memory, min: min}), r}
+		end
+	end
+	defp import_vec_item_type(t, <<0x1, 0x70, 0, p::binary>>) do
+		{m, r} = varint_size(p)
+		{Map.merge(t, %{type: :table, min: m}), r}
+	end
+	defp import_vec_item_type(t, <<0x1, 0x70, 1, p::binary>>) do
+		{min, r} = varint_size(p)
+		{max, s} = varint_size(r)
+		{Map.merge(t, %{type: :table, min: min, max: max}), s}
 	end
 
 	defp decode_section(module, <<>>) do
@@ -62,11 +127,14 @@ defmodule Windtrap do
 
 	defp decode_custom(module), do: module
 	defp decode_types(module) do
-		module
+		Map.put(module, :types, vec(module.sections[@section_types_id]))
 	end
 
 	defp decode_imports(module) do
-		module
+		section = module.sections[@section_imports_id]
+		imports = import_vec(section)
+		
+		Map.put(module, :imports, imports)
 	end
 
 	defp decode_functions(module) do
--- test/windtrap_test.exs
+++ test/windtrap_test.exs
@@ -1,8 +1,51 @@
 defmodule WindtrapTest do
-  use ExUnit.Case
-  doctest Windtrap
+	use ExUnit.Case
+	doctest Windtrap
 
-  test "greets the world" do
-    assert Windtrap.hello() == :world
-  end
+	test "single-byte varint" do
+		{val, <<extra :: big-size(32)>>} = Windtrap.varint_size(<<0x72, 0xde, 0xad, 0xbe, 0xef>>)
+		assert val == 0x72
+		assert extra == 0xdeadbeef
+	end
+
+	test "multi-byte varint" do
+		{val, <<extra :: big-size(32)>>} = Windtrap.varint_size(<<0x83, 0x15, 0xde, 0xad, 0xbe, 0xef>>)
+		assert val == 0xa83
+		assert extra == 0xdeadbeef
+	end
+
+	test "complex when it should be simple" do
+		{val, <<extra :: binary>>} = Windtrap.varint_size(<<0x8d, 0x80, 0x80, 0x80, 0x00>>)
+		assert val == 13
+		assert String.length(extra) == 0
+	end
+
+	test "unfurl invalid vector" do
+		assert_raise FunctionClauseError, fn ->
+			Windtrap.vec(<<>>)
+		end
+	end
+
+	test "unfurl empty vector" do
+		{sig, _} = Windtrap.vec(<<0>>)
+		assert {} = sig
+	end
+
+	test "unfurl single-value vector" do
+		{sig, _} = Windtrap.vec(<<01, 0x7f>>)
+		assert {:i32} = sig
+	end
+
+	test "unfurl multiple-value vector" do
+		{sig, _} = Windtrap.vec(<<03, 0x7f, 0x7f, 0x7f>>)
+		assert {:i32, :i32, :i32} == sig
+	end
+
+	test "unfurl function vector" do
+		{sig, _} = Windtrap.vec(<<3, 0x60, 1, 0x7f, 1, 0x7f, 0x60, 0, 1, 0x7f, 0x60, 0, 0>>)
+		assert tuple_size(sig) == 3
+		assert elem(sig, 0) == {{:i32}, {:i32}}
+		assert elem(sig, 1) == {{}, {:i32}}
+		assert elem(sig, 2) == {{}, {}}
+	end
 end

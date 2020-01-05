6661c7929adac28dda361d73cf7a01e652070c19
Guillaume Ballet <gballet@gmail.com>
2020-01-05 21:00:32 +0100

    Add instruction table to the disassembler

--- lib/windtrap/disassembler.ex
+++ lib/windtrap/disassembler.ex
@@ -9,6 +9,44 @@ defmodule Windtrap.Disassembler do
 	"""
 
 	import Windtrap.Varint
+	@instructions [
+		# Control instructions, 0x00..
+		"unreachable", "nop", "block", "loop", "if", "else",
+		"gap6", "gap7", "gap8", "gap9", "gapa",
+		"end", "br", "br_if", "br_table", "return", "call", "call_indirect",
+
+		"gap12", "gap13", "gap14", "gap15", "gap16", "gap17", "gap18", "gap19",
+
+		# Parametric instructions 0x1a..
+		"drop", "select",
+
+		"gap1c", "gap1d", "gap1e", "gap1f",
+
+		# Variable instructions 0x20...
+		"get_local", "set_local", "tee_local", "get_global", "set_global",
+
+		"gap25", "gap26", "gap27", "gap28", "gap29", "gap2a", "gap2b", "gap2c", "gap2d", "gap2e", "gap2f",
+
+		# Memory instructions 0x28...
+		"i32.load", "i64.load", "f32.load", "f64.load", "i32.load8_s", "i32.load8_u", "i32.load16_s", "i32.load16_u", "i64.load8_s",
+		"i64.load8_u", "i64.load16_s", "i64.load16_u", "i64.load32_s", "i64.load32_u", "i32.store", "i64.store", "f32.store",
+		"f64.store", "i32.store8", "i32.store16", "i64.store8", "i64.store16", "i64.store32", "memory.size", "memory.grow",
+
+		# Const instructions 0x40...
+		"i32.const", "i64.const", "f32.const", "f64.const",
+
+		# Numeric instructions 0x45...
+		"i32.eqz", "i32.eq", "i32.ne", "i32.lt_s", "i32.lt_u", "i32.gt_s", "i32.gt_u", "i32.le_s", "i32.le_u", "i32.ge_s", "i32.ge_u",
+    "i64.eqz", "i64.eq", "i64.ne", "i64.lt_s", "i64.lt_u", "i64.gt_s", "i64.gt_u", "i64.le_s", "i64.le_u", "i64.ge_s", "i64.ge_u",
+		"f32.eq", "f32.ne", "f32.lt", "f32.gt", "f32.le", "f32.ge",
+		"f64.eq", "f64.ne", "f64.lt", "f64.gt", "f64.le", "f64.ge",
+		"i32.clz", "i32.ctz", "i32.popcnt", "i32.add", "i32.sub", "i32.mul", "i32.div_s", "i32.div_u", "i32.rem_s", "i32.rem_u", "i32.and", "i32.or", "i32.xor", "i32.shl", "i32.shr_s", "i32.shr_u", "i32.rotl", "i32.rotr",
+		"i64.clz", "i64.ctz", "i64.popcnt", "i64.add", "i64.sub", "i64.mul", "i64.div_s", "i64.div_u", "i64.rem_s", "i64.rem_u", "i64.and", "i64.or", "i64.xor", "i64.shl" ,"i64.shr_s", "i64.shr_u", "i64.rotl", "i64.rotr",
+		"f32.abs", "f32.neg", "f32.ceil", "f32.floor", "f32.trunc", "f32.nearest", "f32.sqrt", "f32.add", "f32.sub", "f32.mul", "f32.div", "f32.min", "f32.max", "f32.copysign",
+		"f64.abs", "f64.neg", "f64.ceil", "f64.floor", "f64.trunc", "f64.nearest", "f64.sqrt", "f64.add", "f64.sub", "f64.mul", "f64.div", "f64.min", "f64.max", "f64.copysign",
+		"i32.wrap/i64", "i32.trunc_s/f32", "i32.trunc_u/f32", "i32.trunc_s/f64", "i32.trunc_u/f64", "i64.extend_s/i32", "i64.extend_u/i32", "i64.trunc_s/f32", "i64.trunc_u/f32", "i64.trunc_s/f64", "i64.trunc_u/f64", "f32.convert_s/i32", "f32.convert_u/i32", "f32.convert_s/i64", "f32.convert_u/i64", "f32.demote/f64", "f64.convert_s/i32", "f64.convert_u/i32", "f64.convert_s/i64", "f64.convert_u/i64", "f64.promote/f32", "i32.reinterpret/f32", "i64.reinterpret/f64", "f32.reinterpret/i32", "f64.reinterpret/i64",
+	] |> Enum.with_index
+	|> Enum.reduce(%{}, fn {sym, idx}, acc -> Map.put(acc, idx, String.to_atom(sym)) end)
 
 	@numeric_instructions Enum.reduce [
 		"i32.eqz", "i32.eq", "i32.ne", "i32.lt_s", "i32.lt_u", "i32.gt_s", "i32.gt_u", "i32.le_s", "i32.le_u", "i32.ge_s", "i32.ge_u",

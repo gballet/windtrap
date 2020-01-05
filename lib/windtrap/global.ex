dc12765f380cd624b3b7117ea92fd97259ea5fb6
Guillaume Ballet <gballet@gmail.com>
2020-01-05 20:12:12 +0100

    Apply linter in modules.ex and global.ex

--- lib/windtrap/global.ex
+++ lib/windtrap/global.ex
@@ -1,7 +1,7 @@
 defmodule Windtrap.Global do
   defstruct const: false,
-            type: :none,
-            value_type: :none,
-            value: 0,
-            expr: <<>>
+    type: :none,
+    value_type: :none,
+    value: 0,
+    expr: <<>>
 end
--- lib/windtrap/module.ex
+++ lib/windtrap/module.ex
@@ -1,23 +1,19 @@
 defmodule Windtrap.Module do
-	@moduledoc """
-	Represents a WASM module
-	"""
+  @moduledoc """
+  Represents a WASM module
+  """
 
-	# @enforce_keys: [:types]
-	defstruct imports: {},
-						exports: {},
-						types: {},
-						# types of each function implemented by this module
-						function_types: {},
-						sections: %{},
-						# List of function descriptors
-						functions: %{},
-						# types of each imported function, followed by those of
-						# functions implemented by this module
-						function_index: {},
-						memory: {},
-						code: <<>>,
-						globals: %{},
-						# List of resolved modules, keyed by module name
-						dependencies: %{}
+  # @enforce_keys: [:types]
+  defstruct imports: {},
+    exports: {},
+    types: {},
+    function_types: {},   # types of each function implemented by this module
+    sections: %{},
+    functions: %{},       # List of function descriptors
+    function_index: {},   # types of each imported function, followed by those of
+			  # functions implemented by this module
+    memory: {},
+    code: <<>>,
+    globals: %{},
+    dependencies: %{}     # List of resolved modules, keyed by module name
 end
